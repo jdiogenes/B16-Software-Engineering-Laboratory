5da1990865e7a4acd6d6fd3be7bde1c38fefabca
Juscelino Diógenes Santana <jdsantana0@gmail.com>
2017-05-02 15:00:28 -0300

    Adicionada a opção de modificar a posição inicial x e y da bola.

--- test-ball.cpp
+++ test-ball.cpp
@@ -9,6 +9,11 @@ int main(int argc, char** argv)
 {
   Ball ball ;
   const double dt = 1.0/30 ;
+  cout << "A coordenada x padrão da bola é " << x << ". Digite o novo valor da coordenada x: (digite " << x << " para manter o padrão) " << endl; 
+  cin >> x;
+  cout << "A coordenada y padrão da bola é " << y << ". Digite o novo valor da coordenada y: (digite " << y << " para manter o padrão) " << endl; 
+  cin >> y;	
+
   for (int i = 0 ; i < 100 ; ++i) {
     ball.step(dt) ;
     ball.display() ;
